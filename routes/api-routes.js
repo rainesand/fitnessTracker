cbf906f0ddb3c38eedd50e9720fc04bf24d05ffb
Andrew Raines <rainesand26@gmail.com>
2020-09-17 19:59:52 -0400

    added models, routes, and servers

---
+++ routes/api-routes.js
@@ -0,0 +1,62 @@
+const db = require("../models");
+
+module.exports = function (app) {
+
+    db.Workout.find({}).then(function (res) {
+        if (res.length === 0) {
+            console.log("there's nothing to see here");
+            require("./seeders/seed.js");
+        }
+    });
+
+
+
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({}).then(dbWorkout => {
+            console.log(dbWorkout);
+            dbWorkout.forEach(workout => {
+                var total = 0;
+                workout.exercises.forEach(e => {
+                    total += e.duration;
+                });
+                workout.totalDuration = total;
+            });
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
+
+
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findOneAndUpdate(
+            {_id: req.params.id},
+            {
+                $inc: {totalDuration: req.body.duration},
+                $push: {exercises: req.body}
+            },
+            {new: true}).then(dbWorkout => {
+                res.json(dbWorkout);
+            }).catch(err => {
+                res.json(err);
+            });
+    });
+
+    app.post("/api/workouts", ({body}, res) => {
+        console.log(body);
+        db.Workout.create(body).then((dbWorkout => {
+            res.json(dbWorkout);
+        })).catch(err => {
+            res.json(err);
+        });
+    });
+
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find({}).then(dbWorkout => {
+            console.log(dbWorkout);
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
+}
